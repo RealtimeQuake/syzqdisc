8c502606e69974ade93de3794d143c6df0ba74ea
Unbounded Mesa <unboundedmesa@gmail.com>
2026-02-20 12:52:54 +0100

    Advanced config for bypass resource limitation

--- executor/enhanced_qdisc.h
+++ executor/enhanced_qdisc.h
@@ -1,3 +1,59 @@
+
+#define MACRO_SYZ_ADDQDISC_ROOT(type, name)                                 \
+static long syz_addqdisc_root_##name(volatile long opts, volatile long size)\
+{                                                                           \
+        return pseudo_addqdisc(type, ROOT_ID, opts, size);                        \
+}                                                                           \
+
+#define MACRO_SYZ_ADDQDISC(type, name)                                                       \
+static long syz_addqdisc_##name(volatile long parent, volatile long opts, volatile long size)\
+{                                                                                            \
+        return pseudo_addqdisc(type, parent, opts, size);                                          \
+}                                                                                            \
+
+
+#define MACRO_SYZ_ADDCLASS(type, name)                                                                                \
+static long syz_addclass_##name(volatile long portid, volatile long class_num, volatile long opts, volatile long size)\
+{                                                                                                                     \
+        return pseudo_addclass(type, portid, class_num, opts, size);                                                                    \
+}                                                                                                                      \
+
+
+
+#define MACRO_SYZ_CHANGEQDISC(type, name)                                                       \
+static long syz_changeqdisc_##name(volatile long handle, volatile long opts, volatile long size)\
+{                                                                                               \
+        return pseudo_changeqdisc(type, handle, opts, size);                                          \
+}                                                                                               \
+
+#define MACRO_SYZ_CHANGECLASS(type, name)                                                       \
+static long syz_changeclass_##name(volatile long handle, volatile long opts, volatile long size)\
+{                                                                                               \
+        return pseudo_changeclass(type, handle, opts, size);                                          \
+}                                                                                               \
+
+
+#define MACRO_SYZ_DELQDISC(type, name)                  \
+static long syz_delqdisc_##name(volatile long handle)\
+{                                                       \
+        return pseudo_delqdisc(handle);                  \
+}                                                        \
+
+#define MACRO_SYZ_DELCLASS(type, name)\
+static long syz_delclass_##name(volatile long handle)\
+{                                                       \
+        return pseudo_delclass(handle);                 \
+}                                                       \
+
+
+
+#define MACRO_SYZ_SENDPRIO(type, name)                                                        \
+static long syz_sendprio_##name(volatile long handle, volatile long count, volatile long size)\
+{                                                                                             \
+        return pseudo_sendprio(handle, count, size);                                          \
+}                                                                                             \
+
+
 enum qdisc_type {
     Q_UNDEF,
     Q_CAKE,
@@ -39,11 +95,11 @@ typedef void (*qdisc_faill_func)(char* error, int error_num);
 extern "C" void __setup_qdisc(const char* arg_ifname, const qdisc_faill_func func);
 extern "C" void __reset_qdisc();
 
-extern "C" long pseudo_addqdisc(volatile long parent, volatile long opts, volatile long size);
+extern "C" long pseudo_addqdisc(volatile long type, volatile long parent, volatile long opts, volatile long size);
 extern "C" long pseudo_addclass(volatile long type, volatile long portid, volatile long class_num, volatile long opt, volatile long opt_size);
 
-extern "C" long pseudo_changeqdisc(volatile long opts, volatile long size);
-extern "C" long pseudo_changeclass(volatile long opts, volatile long size);
+extern "C" long pseudo_changeqdisc(volatile long type, volatile long handle, volatile long opts, volatile long size);
+extern "C" long pseudo_changeclass(volatile long type, volatile long handle, volatile long opts, volatile long size);
 
 extern "C" long pseudo_delqdisc(volatile long handle);
 extern "C" long pseudo_delclass(volatile long handle);
@@ -67,111 +123,77 @@ static void setup_qdisc()
     __setup_qdisc("lo", &error);
 }
 
-#if SYZ_EXECUTOR || __NR_syz_addqdisc_root
 
-static long syz_addqdisc_root(volatile long opts, volatile long size)
-{
-    return pseudo_addqdisc(ROOT_ID, opts, size);
-}
-#endif
+MACRO_SYZ_ADDQDISC_ROOT(Q_CBS,   cbs)
+MACRO_SYZ_ADDQDISC_ROOT(Q_NETEM, netem)
+MACRO_SYZ_ADDQDISC_ROOT(Q_RED,   red)
+MACRO_SYZ_ADDQDISC_ROOT(Q_SFB,   sfb)
+MACRO_SYZ_ADDQDISC_ROOT(Q_TBF,   tbf)
+MACRO_SYZ_ADDQDISC_ROOT(Q_DRR,   drr)
+MACRO_SYZ_ADDQDISC_ROOT(Q_ETS,   ets)
+MACRO_SYZ_ADDQDISC_ROOT(Q_HFSC,  hfsc)
+MACRO_SYZ_ADDQDISC_ROOT(Q_HTB,   htb)
+MACRO_SYZ_ADDQDISC_ROOT(Q_PRIO,  prio)
+MACRO_SYZ_ADDQDISC_ROOT(Q_QFQ,   qfq)
 
-#if SYZ_EXECUTOR || __NR_syz_addqdisc
+MACRO_SYZ_ADDQDISC(Q_CAKE,  cake)
+MACRO_SYZ_ADDQDISC(Q_CBS,   cbs)
+MACRO_SYZ_ADDQDISC(Q_NETEM, netem)
+MACRO_SYZ_ADDQDISC(Q_RED,   red)
+MACRO_SYZ_ADDQDISC(Q_SFB,   sfb)
+MACRO_SYZ_ADDQDISC(Q_TBF,   tbf)
+MACRO_SYZ_ADDQDISC(Q_DRR,   drr)
+MACRO_SYZ_ADDQDISC(Q_ETS,   ets)
+MACRO_SYZ_ADDQDISC(Q_HFSC,  hfsc)
+MACRO_SYZ_ADDQDISC(Q_HTB,   htb)
+MACRO_SYZ_ADDQDISC(Q_PRIO,  prio)
+MACRO_SYZ_ADDQDISC(Q_QFQ,   qfq)
 
-static long syz_addqdisc(volatile long parent, volatile long opts, volatile long size)
-{
-    return pseudo_addqdisc(parent, opts, size);
-}
-#endif
+MACRO_SYZ_ADDCLASS(Q_DRR, drr)
+MACRO_SYZ_ADDCLASS(Q_ETS, ets)
+MACRO_SYZ_ADDCLASS(Q_HFSC, hfsc)
+MACRO_SYZ_ADDCLASS(Q_HTB, htb)
+MACRO_SYZ_ADDCLASS(Q_PRIO, prio)
+MACRO_SYZ_ADDCLASS(Q_QFQ, qfq)
 
+MACRO_SYZ_CHANGEQDISC(Q_CAKE, cake)
+MACRO_SYZ_CHANGEQDISC(Q_CBS, cbs)
+MACRO_SYZ_CHANGEQDISC(Q_NETEM, netem)
+MACRO_SYZ_CHANGEQDISC(Q_RED, red)
+MACRO_SYZ_CHANGEQDISC(Q_SFB, sfb)
+MACRO_SYZ_CHANGEQDISC(Q_TBF, tbf)
+MACRO_SYZ_CHANGEQDISC(Q_ETS, ets)
+MACRO_SYZ_CHANGEQDISC(Q_PRIO, prio)
 
-#if SYZ_EXECUTOR || __NR_syz_addclass_drr
-
-static long syz_addclass_drr(volatile long port_qdisc, volatile long class_num, volatile long opts, volatile long size)
-{
-    return pseudo_addclass(Q_DRR, port_qdisc, class_num, opts, size);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_addclass_ets
-
-static long syz_addclass_ets(volatile long port_qdisc, volatile long class_num, volatile long opts, volatile long size)
-{
-    return pseudo_addclass(Q_ETS, port_qdisc, class_num, opts, size);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_addclass_hfsc
-
-static long syz_addclass_hfsc(volatile long port_qdisc, volatile long class_num, volatile long opts, volatile long size)
-{
-    return pseudo_addclass(Q_HFSC, port_qdisc, class_num, opts, size);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_addclass_htb
-
-static long syz_addclass_htb(volatile long port_qdisc, volatile long class_num, volatile long opts, volatile long size)
-{
-    return pseudo_addclass(Q_HTB, port_qdisc, class_num, opts, size);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_addclass_prio
-
-static long syz_addclass_prio(volatile long port_qdisc, volatile long class_num, volatile long opts, volatile long size)
-{
-    return pseudo_addclass(Q_PRIO, port_qdisc, class_num, opts, size);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_addclass_qfq
-
-static long syz_addclass_qfq(volatile long port_qdisc, volatile long class_num, volatile long opts, volatile long size)
-{
-    return pseudo_addclass(Q_QFQ, port_qdisc, class_num, opts, size);
-}
-#endif
+MACRO_SYZ_CHANGECLASS(Q_DRR, drr)
+MACRO_SYZ_CHANGECLASS(Q_HFSC, hfsc)
+MACRO_SYZ_CHANGECLASS(Q_HTB, htb)
+MACRO_SYZ_CHANGECLASS(Q_QFQ, qfq)
+MACRO_SYZ_CHANGECLASS(Q_ETS, ets)
 
 
+MACRO_SYZ_DELQDISC(Q_CAKE, cake)
+MACRO_SYZ_DELQDISC(Q_CBS, cbs)
+MACRO_SYZ_DELQDISC(Q_NETEM, netem)
+MACRO_SYZ_DELQDISC(Q_RED, red)
+MACRO_SYZ_DELQDISC(Q_SFB, sfb)
+MACRO_SYZ_DELQDISC(Q_TBF, tbf)
+MACRO_SYZ_DELQDISC(Q_DRR, drr)
+MACRO_SYZ_DELQDISC(Q_ETS, ets)
+MACRO_SYZ_DELQDISC(Q_HFSC, hfsc)
+MACRO_SYZ_DELQDISC(Q_HTB, htb)
+MACRO_SYZ_DELQDISC(Q_PRIO, prio)
+MACRO_SYZ_DELQDISC(Q_QFQ, qfq)
 
-#if SYZ_EXECUTOR || __NR_syz_changeqdisc
-
-static long syz_changeqdisc(volatile long opts, volatile long size)
-{
-    return pseudo_changeqdisc(opts, size);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_changeclass
-
-static long syz_changeclass(volatile long opts, volatile long size)
-{
-    return pseudo_changeclass(opts, size);
-}
-#endif
-
-
-#if SYZ_EXECUTOR || __NR_syz_delqdisc
-
-static long syz_delqdisc(volatile long handle)
-{
-    return pseudo_delqdisc(handle);
-}
-#endif
-
-#if SYZ_EXECUTOR || __NR_syz_delclass
-
-static long syz_delclass(volatile long handle)
-{
-    return pseudo_delclass(handle);
-}
-#endif
+MACRO_SYZ_DELCLASS(Q_DRR, drr)
+MACRO_SYZ_DELCLASS(Q_HFSC, hfsc)
+MACRO_SYZ_DELCLASS(Q_HTB, htb)
+MACRO_SYZ_DELCLASS(Q_QFQ, qfq)
 
 
-#if SYZ_EXECUTOR || __NR_syz_sendprio
-
-static long syz_sendprio(volatile long handle, volatile long count, volatile long size)
-{
-    return pseudo_sendprio(handle, count, size);
-}
-#endif
+MACRO_SYZ_SENDPRIO(Q_DRR, drr)
+MACRO_SYZ_SENDPRIO(Q_ETS, ets)
+MACRO_SYZ_SENDPRIO(Q_HFSC, hfsc)
+MACRO_SYZ_SENDPRIO(Q_HTB, htb)
+MACRO_SYZ_SENDPRIO(Q_PRIO, prio)
+MACRO_SYZ_SENDPRIO(Q_QFQ, qfq)
